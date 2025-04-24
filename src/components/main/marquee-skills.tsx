ef6e5b2bbfe1d7d8e53d81a8f4167b8af3ceef11
Luis Chrestia <chrestia.luis@gmail.com>
2025-04-24 11:12:56 -0300

    fix: marque habilidades principales mas al centro, text-nowrap en la card

--- src/components/main/marquee-skills.tsx
+++ src/components/main/marquee-skills.tsx
@@ -2,11 +2,7 @@ import { cn } from "@/lib/utils";
 import { Marquee } from "@/components/magicui/marquee";
 import { Skills } from "@/types";
 
-const ReviewCard = ({
-  skill
-}: {
-  skill: string;
-}) => {
+const ReviewCard = ({ skill }: { skill: string }) => {
   return (
     <figure
       className={cn(
@@ -14,44 +10,43 @@ const ReviewCard = ({
         // light styles
         "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
         // dark styles
-        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]",
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
       )}
     >
       <div className="flex flex-row items-center gap-2">
         {/* Aqui pondria el svg o imagen*/}
-        <div className="text-center w-full">
-          {skill}
-        </div>
+        <div className="text-center text-nowrap w-full">{skill}</div>
       </div>
-      
     </figure>
   );
 };
 
-export function Marquee3D({data}: {data: Skills}) {
+export function Marquee3D({ data }: { data: Skills }) {
   return (
     <>
-      <h2 className="text-4xl font-black text-center mb-4">Habilidades Técnicas</h2>
+      <h2 className="text-4xl font-black text-center mb-4">
+        Habilidades Técnicas
+      </h2>
       <div className="relative flex h-96 w-full flex-row items-center justify-center gap-4 overflow-hidden [perspective:500px]">
         <div className="flex flex-row items-center gap-4 -translate-x-10 translate-y-0 -translate-z-28 rotate-x-[20deg] -rotate-y-12 rotate-z-[20deg]">
           <Marquee pauseOnHover vertical className="[--duration:15s]">
             {data.principales.map((review) => (
-              <ReviewCard key={review} skill={review}/>
+              <ReviewCard key={review} skill={review} />
             ))}
           </Marquee>
           <Marquee reverse pauseOnHover className="[--duration:15s]" vertical>
             {data.secundarias.map((review) => (
-              <ReviewCard key={review} skill={review}/>
+              <ReviewCard key={review} skill={review} />
             ))}
           </Marquee>
           <Marquee pauseOnHover className="[--duration:15s]" vertical>
-            {data.otros_conocimientos.map((review) => (
-              <ReviewCard key={review} skill={review}/>
+            {data.principales.map((review) => (
+              <ReviewCard key={review} skill={review} />
             ))}
           </Marquee>
           <Marquee reverse pauseOnHover className="[--duration:15s]" vertical>
-            {data.principales.map((review) => (
-              <ReviewCard key={review} skill={review}/>
+            {data.otros_conocimientos.map((review) => (
+              <ReviewCard key={review} skill={review} />
             ))}
           </Marquee>
         </div>
