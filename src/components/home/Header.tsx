20907d57adf769482c21108e73070237f04bceaf
Luis Chrestia <chrestia.luis@gmail.com>
2025-04-27 12:25:13 -0300

    update: cambiando animacion debajo del nombre

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -3,7 +3,7 @@ import { CircleLanguages } from "@/components/circle-languages";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 import { FaGithub, FaLinkedin, FaMailBulk } from "react-icons/fa";
 import { HyperText } from "@/components/magicui/hyper-text";
-import { WordRotate } from "@/components/magicui/word-rotate";
+import { MorphingText } from "@/components/magicui/morphing-text";
 
 export default function Header({ alumn } : { alumn: string }) {
   function handleLink(url: string) {
@@ -12,11 +12,11 @@ export default function Header({ alumn } : { alumn: string }) {
 
   return (
     <header className="relative min-h-[500px] grid grid-cols-2 gap-y-2 max-w-7xl mx-auto">
-      <div className="order-2 md:order-1 col-span-2 md:col-span-1 m-auto w-full px-4 md:px-12">
+      <div className="order-2 md:order-1 col-span-2 md:col-span-1 m-auto w-full ps-4 md:ps-12 z-[9999]">
         <HyperText animateOnHover={false} className="text-3xl md:text-7xl font-extrabold py-0" >{alumn}</HyperText>
-        <WordRotate
-          className="text-lg md:text-2xl text-black dark:text-white min-w-full w-full"
-          words={["Técnico Universitario en Programación", "Desarrollador web Fullstack"]}
+        <MorphingText
+          className="text-start text-black dark:text-white min-w-full w-full"
+          texts={["Técnico Universitario en Programación", "Desarrollador Web Fullstack"]}
         />
         <div className="pt-3 space-x-4">
           <InteractiveHoverButton className="border-blue-600" onClick={() => handleLink("https://github.com/chLuis")}><FaGithub  /></InteractiveHoverButton>
